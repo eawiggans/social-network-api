f48e830610eb66a8c9c658d5dee8f31b4116aa65
Emma Wiggans <ea.wiggans@gmail.com>
2023-01-16 13:14:42 -0500

    created a few test CRUD operators in controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,11 +1,29 @@
-const Thought = require('../models/Thought');
+const { Thought, User, Reaction } = require('../models');
 
-//module.exports =
-
+module.exports = {
 //get thought by id
-
+    getThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .then((thought) => 
+        !thought
+        ? res.status(404).json({ message: 'No thought with this ID'})
+        : res.json(thought));
+    },
 //post new thought
-
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thought) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $addToSet: { thoughts: thought._id }},
+                { new: true }
+            );
+        })
+        .then((user) => 
+        !user 
+        ? res.status(404).json({ message: 'Thought created, but found no user with this ID'})
+        : res.json('Created new thought for this user'));
+    }
 //update/put thought by id
 
 //delete thought by id
@@ -13,3 +31,6 @@ const Thought = require('../models/Thought');
 //post reaction
 
 //delete reaction
+
+}
+
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,27 @@
 const User = require('../models/User');
 
-//module.exports =
-
+module.exports = {
 //get all users
-
+    getUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
+//get single user by id
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId})
+        .then((user) =>
+        !user
+        ? res.status(404).json({ message: 'No user with this ID'})
+        : res.json(user))
+        .catch((err) => res.status(500).json(err));
+    },
 //post new user
-
-//get user by id
+    createUser(req, res) {
+        User.create(req.body)
+        .then((socialNetworkDB) => res.json(socialNetworkDB))
+        .catch((err) => res.status(500).json(err))
+    },
 
 //update user by id
 
@@ -15,3 +30,7 @@ const User = require('../models/User');
 //post friend by id
 
 //delete friend by id
+
+}
+
+
